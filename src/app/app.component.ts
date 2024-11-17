b65c2dd3aef4b32bfb2a772f2974e3a926571fd7
MiguelBarconte <barcontemiguel23@gmail.com>
2024-11-17 19:48:09 -0300

    Agregando pagina temporadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogoComponent } from "./catalogo/catalogo.component";
 import { BuscadorComponent } from './buscador/buscador.component';
+import { TemporadaComponent } from './pages/temporada/temporada.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CatalogoComponent, BuscadorComponent],
+  imports: [CatalogoComponent, BuscadorComponent, TemporadaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/temporada/temporada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temporada',
+  standalone: true,
+  imports: [],
+  templateUrl: './temporada.component.html',
+  styleUrl: './temporada.component.scss'
+})
+export class TemporadaComponent {
+
+}
